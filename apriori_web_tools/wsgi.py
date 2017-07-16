2ef2582d45e899a518e0630a980068818606b866
Apriorhythm <apriorhythm@outlook.com>
2017-07-16 20:57:55 +0800

    change for uwsgi

--- apriori_web_tools/wsgi.py
+++ apriori_web_tools/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apriori_web_tools.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "apriori_web_tools.settings.database")
 
 application = get_wsgi_application()
